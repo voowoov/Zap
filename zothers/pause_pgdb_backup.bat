49fcc4cc9703fb32bbb85042d0155336261b0c6c
Voowoov <etiennerdev@gmail.com>
2023-09-25 12:34:05 -0400

    m

--- zothers/pause_pgdb_backup.bat
+++
@@ -1,3 +0,0 @@
-docker pause pgdb_zap
-timeout 120
-docker unpause pgdb_zap
--- zothers/renew.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-
-cd /home/ubuntu/erSite
-/usr/local/bin/docker-compose -f docker-compose.deploy.yml run --rm certbot certbot renew
