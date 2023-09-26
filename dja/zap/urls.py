b69b3c49a798f7b14624ddb1ad9b0eb07a312b8f
Voowoov <etiennerdev@gmail.com>
2023-09-25 21:33:23 -0400

    m

--- dja/zap/urls.py
+++ dja/zap/urls.py
@@ -53,6 +53,6 @@ urlpatterns += i18n_patterns(
     # path("account/", include("django.contrib.auth.urls")),
 )
 
-if settings.DEBUG:
+if settings.DEBUG and not settings.PROD:
     # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT_DEV)
